7d5421502fbe78dd9e837d1a3bf4f5ae8d5aeb6f
manekmehra <61108498+manekmehra@users.noreply.github.com>
2020-04-25 10:58:09 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,40 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.Smokeimage = loadImage("sprites/smoke.png")
+    this.trajectory = []
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    if (this.body.velocity.x>8 && this.body.position.x>250){
+var position = [this.body.position.x,this.body.position.y]
+    this.trajectory.push(position)}
+    for (var i = 0; i<this.trajectory.length; i++){
+      image(this.Smokeimage,this.trajectory[i][0],this.trajectory[i][1])
+    }
+    super.display();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
